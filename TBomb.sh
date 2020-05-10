37c6b68812c1815f41625f89d74531bfef352191
Aveng-hacker <61612610+sujoybiswas@users.noreply.github.com>
2020-05-11 00:41:43 +0530

    Update TBomb.sh

--- TBomb.sh
+++ TBomb.sh
@@ -14,7 +14,7 @@ echo .
 apt install figlet toilet python curl -y
 apt install python3-pip
 pip install -r requirements.txt
-echo This Script Was Made By Aveng >update.speedx
+echo This Script Was Made By SpeedX >update.speedx
 echo Requirements Installed....
 echo Press Enter To Continue...
 read upd
@@ -27,10 +27,10 @@ echo -e "\e[1;31m"
 figlet TBomb
 echo -e "\e[1;34m Created By \e[1;32m"
 toilet -f mono12 -F border SpeedX
-echo -e "\e[4;34m This Bomber Was Created By Aveng \e[0m"
+echo -e "\e[4;34m This Bomber Was Created By SpeedX \e[0m"
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
-echo -e "\e[1;32m           Mail: sundersujoy.sb@gmail.com \e[0m"
-echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/Aveng-hacker \e[0m"
+echo -e "\e[1;32m           Mail: ggspeedx29@gmail.com \e[0m"
+echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/GyanaTech \e[0m"
 echo " "
 echo -e "\e[4;31m Please Read Instruction Carefully !!! \e[0m"
 echo " "
@@ -58,7 +58,7 @@ elif [ $ch -eq 3 ];then
 clear
 apt install git -y
 echo -e "\e[1;34m Downloading Latest Files..."
-git clone https://github.com/sujoybiswas/TBomb
+git clone https://github.com/TheSpeedX/TBomb
 if [[ -s TBomb/TBomb.sh ]];then
 cd TBomb
 cp -r -f * .. > temp
@@ -90,7 +90,7 @@ echo "  [+] Automated Future Updates"
 echo "  [+] Easy To Use And Embed in Code"
 echo -e "\e[1;32m                   Contributors\e[1;33m"
 echo -e "\e[1;33m      [*]  SpeedX   \e[1;31m"
-echo "         [-] Mail At: sundersujoy.sb@gmail.com"
+echo "         [-] Mail At: ggspeedx29@gmail.com"
 echo -e "\e[1;33m      [*]  The Black Hacker Roxstar   \e[1;31m"
 echo "         [-] Ping At: http://wa.me/917600140353"
 echo -e "\e[1;33m      [*]  Rieltar   \e[1;31m"
@@ -106,8 +106,8 @@ echo -e "\e[1;32m Make Sure To Update it If It Does not Work.\e[0m"
 echo  " "
 echo -e "\e[4;31m That's All !!!\e[0m"
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
-echo -e "\e[1;32m           Mail: sundersujoy.sb@gmail.com \e[0m"
-echo -e "\e[1;32m       Whatsapp: https://bit.do/Aveng-hacker \e[0m"
+echo -e "\e[1;32m           Mail: ggspeedx29@gmail.com \e[0m"
+echo -e "\e[1;32m       Whatsapp: https://bit.do/speedxgit \e[0m"
 echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/Aveng-Hacker \e[0m"
 echo "Press Enter To Go Home"
 read a3
@@ -119,9 +119,9 @@ figlet TBomb
 echo -e "\e[1;34m Created By \e[1;32m"
 toilet -f mono12 -F border SpeedX
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
-echo -e "\e[1;32m           Mail: sundersujoy.sb@gmail.com \e[0m"
-echo -e "\e[1;32m       Whatsapp: https://bit.do/speedxgit \e[0m"
-echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/Aveng-hacker \e[0m"
+echo -e "\e[1;32m           Mail: Sundersujoy.sb@gmail.com \e[0m"
+echo -e "\e[1;32m       Whatsapp: https://bit.do/Aveng-Hacker \e[0m"
+echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/Aveng-Hacker \e[0m"
 echo " "
 exit 0
 else
